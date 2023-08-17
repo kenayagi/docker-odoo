f5eee15b11410c6989d50c21668793b8104d88c4
K <luca@pretto.eu>
2023-08-17 11:22:12 +0200

    upd

--- run.sh
+++ run.sh
@@ -9,14 +9,16 @@ sed -i "/^admin_passwd/c\admin_passwd = $ODOO_ADMIN_PASSWD" $ODOO_CONF_FILE
 if [ -f "$ODOO_REQ_FILE" ]; then
     pip install --pre --user --upgrade -r $ODOO_REQ_FILE
     mkdir -p $ODOO_HOMEDIR/setup
-    mv $ODOO_REQ_FILE $ODOO_HOMEDIR/setup/pip-modules-installed-on-`date +%y%m%d`-at-`date +%H%M%S`.txt
+    pip freeze --all | sort > $ODOO_HOMEDIR/setup/$NOW.requirements_installed.txt
+    mv $ODOO_REQ_FILE $ODOO_HOMEDIR/setup/$NOW.requirements.txt
 fi
 
 if [ -f "$ODOO_UPD_FILE" ]; then
     /usr/local/bin/odoo --data-dir=$ODOO_HOMEDIR/data_dir --config=$ODOO_CONF_FILE --database=$ODOO_DB --db_host=$POSTGRES_HOST --db_user=$POSTGRES_USER --db_password=$POSTGRES_PASSWORD \
     --update=$(< $ODOO_UPD_FILE) --load-language=it_IT --i18n-overwrite --workers=0 --stop-after-init
     mkdir -p $ODOO_HOMEDIR/setup
-    mv $ODOO_UPD_FILE $ODOO_HOMEDIR/setup/odoo-modules-updated-on-`date +%y%m%d`-at-`date +%H%M%S`.txt
+    echo "Modules updated on $NOW: $(< $ODOO_UPD_FILE)" >> $ODOO_HOMEDIR/setup/updates.log
+    rm $ODOO_UPD_FILE
 fi
 
 /usr/local/bin/odoo --data-dir=$ODOO_HOMEDIR/data_dir --config=$ODOO_CONF_FILE --database=$ODOO_DB --db_host=$POSTGRES_HOST --db_user=$POSTGRES_USER --db_password=$POSTGRES_PASSWORD \
