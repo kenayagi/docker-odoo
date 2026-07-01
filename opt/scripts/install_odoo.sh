61021d640516b4fe367cbcd16a82df814602e09d
K <luca@pretto.eu>
2026-07-01 09:11:43 +0200

    Revert "Upgrade Odoo only if the commit has changed"
    This reverts commit 6953a68e5a0ec6d4e69fcfb6e1acbd61207a3184.

--- opt/scripts/install_odoo.sh
+++ opt/scripts/install_odoo.sh
@@ -16,23 +16,9 @@ if [ ! -d "$ODOO_SRC_DIR" ]; then
   git clone --depth 1 --branch "$ODOO_VERSION" \
     https://github.com/OCA/OCB.git "$ODOO_SRC_DIR"
 fi
-
-# Get current commit hash in the repository
-current_commit=$(cd "$ODOO_SRC_DIR" && git rev-parse HEAD)
-
-# Sync the repository to the desired commit
 sync_ocb_repo
-
-# Get the new commit hash after sync
-new_commit=$(cd "$ODOO_SRC_DIR" && git rev-parse HEAD)
-
-# Only upgrade if the commit has changed
-if [ "$current_commit" != "$new_commit" ]; then
-    echo "Odoo commit changed from $current_commit to $new_commit, upgrading..."
-    uv_install "$ODOO_SRC_DIR"
-else
-    echo "Odoo commit hasn't changed, skipping upgrade."
-fi
+# TODO: upgrade odoo uv pip package ONLY if desired commit has changed
+uv_install "$ODOO_SRC_DIR"
 
 echo "Installing openupgradelib..."
 uv_install "git+https://github.com/OCA/openupgradelib.git@master"
