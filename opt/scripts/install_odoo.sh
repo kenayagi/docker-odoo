1bb9338408fd74a755aba93f594a02a1ea95f3c3
K <luca@pretto.eu>
2026-07-14 17:39:11 +0200

    Move Odoo upgrade logic to install_odoo.sh script

--- opt/scripts/install_odoo.sh
+++ opt/scripts/install_odoo.sh
@@ -7,6 +7,26 @@ require_env
 
 if [ -f "$ODOO_VENV/bin/odoo" ]; then
     echo "Odoo has already been installed."
+
+    # Get current commit hash in the repository
+    current_commit=$(cd "$ODOO_SRC_DIR" && git rev-parse HEAD)
+    echo "Current Odoo $ODOO_VERSION is at commit $current_commit"
+
+    # Sync the repository to the desired commit
+    sync_ocb_repo
+
+    # Get the new commit hash after sync
+    new_commit=$(cd "$ODOO_SRC_DIR" && git rev-parse HEAD)
+
+    # Only upgrade if the commit has changed
+    if [ "$current_commit" != "$new_commit" ]; then
+        echo "Upgrading Odoo $ODOO_VERSION from $current_commit to $new_commit..."
+        uv_install "$ODOO_SRC_DIR"
+        echo "Odoo has been upgraded."
+    else
+        echo "Odoo $ODOO_VERSION commit hasn't changed, skipping upgrade."
+    fi
+
 else
     source "$ODOO_VENV/bin/activate"
 
